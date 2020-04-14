de3e71215a102b5fdb7e998c29286313f477dab6
copiner <810023494@qq.com>
2020-04-14 21:08:13 +0800

    list

--- room.js
+++ room.js
@@ -6,7 +6,7 @@ let tutor =
     type:'01',
     typeName:'Chinese',
     level:6,
-    limit:1,
+    limit:5,
     status:1,
     supOut:['01'],
     subOut:[],
@@ -18,7 +18,7 @@ let tutor =
     type:'02',
     typeName:'Math',
     level:6,
-    limit:2,
+    limit:5,
     status:1,
     supOut:['02'],
     subOut:[],
@@ -30,7 +30,7 @@ let tutor =
     type:'03',
     typeName:'English',
     level:7,
-    limit:2,
+    limit:5,
     status:1,
     supOut:['03'],
     subOut:[],
@@ -42,7 +42,7 @@ let tutor =
     type:'04',
     typeName:'Science',
     level:7,
-    limit:2,
+    limit:5,
     status:1,
     supOut:['04'],
     subOut:[],
@@ -54,7 +54,7 @@ let tutor =
     type:'05',
     typeName:'Society',
     level:7,
-    limit:2,
+    limit:5,
     status:0,
     supOut:['05'],
     subOut:[],
@@ -66,7 +66,7 @@ let tutor =
     type:'01',
     typeName:'Chinese',
     level:6,
-    limit:2,
+    limit:5,
     status:1,
     supOut:['01'],
     subOut:[],
@@ -78,7 +78,7 @@ let tutor =
     type:'02',
     typeName:'Math',
     level:6,
-    limit:2,
+    limit:5,
     status:1,
     supOut:['02'],
     subOut:[],
@@ -90,7 +90,7 @@ let tutor =
     type:'03',
     typeName:'English',
     level:6,
-    limit:2,
+    limit:5,
     status:1,
     supOut:['03'],
     subOut:[],
@@ -102,7 +102,7 @@ let tutor =
     type:'04',
     typeName:'Science',
     level:7,
-    limit:2,
+    limit:5,
     status:1,
     supOut:['04'],
     subOut:[],
@@ -114,7 +114,7 @@ let tutor =
     type:'05',
     typeName:'Society',
     level:7,
-    limit:2,
+    limit:5,
     status:0,
     supOut:['05'],
     subOut:[],
@@ -138,7 +138,52 @@ for(let i=0; i<room.length; i++){
 }
 
 
-function listRoom(){
-    
+function listRoom(tutor, session){
+    let tol = 0;
+    for(let i=0; i<tutor.length; i++){
+        tol += tutor.limit;
+    }
 
+    if(tol === session.length){
+        
+        console.log("not equal");
+        return;
+        
+    }
+    let temp = 0;
+
+    while(session.length != 0){
+        temp += 1;
+        
+        let result = [];
+        for(let i=0; i<tutor.length; i++){
+            for(let j=0; j<session.length;j++){
+                if(tutor[i].level != session[i][0]){
+                    if(!tutor[i].temp){
+                        tutor[i].temp = 0;
+                    }
+                    if(tutor[i].temp < tutor[i].limit){
+                        if(!session[j].substring(3).indexOf(tutor[i].supOut)){
+                            tutor[i].temp += 1;
+                            result.push(session[j]+" : "+tutor[i].id);
+                            session.splice(j,1);
+                            j--;
+                        }
+                    }
+                }
+            }
+        }
+
+        return result;
+    
+    }
+    
+    
 }
+
+let list =  listRoom(tutor, session);
+
+for (let i = 0; i<list.length; i++){
+    console.log(list[i]+"|");
+}
+
